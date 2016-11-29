14b1e7da27b6557d7461a585ce708f8dcf6ed193
Jacek Kuzemczak <jacek@kuzemczak.co.uk>
2016-11-29 18:49:17 +0000

    Added more tests for vnstat class

--- tests/vnstat/VnstatTest.php
+++ tests/vnstat/VnstatTest.php
@@ -10,7 +10,35 @@ class VnstatTest extends TestCase {
 	public function testListInterfaces() {
 		$interfaces = vnstat::get_interfaces();
 
+		$this->assertInternalType('array', $interfaces);
 		$this->assertNotEmpty($interfaces);
 	}
+
+	private function getTestVnstatInstance(): vnstat {
+		$interfaces = vnstat::get_interfaces();
+
+		return new vnstat($interfaces[0]);
+	}
+
+	// These are essentially placeholder tests until the return values are refactored.
+	// Just call the methods and make sure they return an array for now.
+
+	public function testGetTraffic() {
+		$vnstat = $this->getTestVnstatInstance();
+
+		$traffic = $vnstat->get_traffic();
+
+		$this->assertInternalType('array', $traffic);
+	}
+
+	public function testGetLiveTraffic() {
+		$vnstat = $this->getTestVnstatInstance();
+
+		$live_traffic = $vnstat->get_live_traffic();
+
+		$this->assertInternalType('array', $live_traffic);
+	}
+
+	//
 
 }
