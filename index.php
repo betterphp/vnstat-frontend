0330a777a39f619d8a93f470593b482484d1cb01
Jacek Kuzemczak <jacek@kuzemczak.co.uk>
2016-11-30 22:00:44 +0000

    Implemented index page using new methods

--- index.php
+++ index.php
@@ -4,32 +4,43 @@ declare(strict_types=1);
 
 namespace betterphp\vnstat_frontend;
 
+use betterphp\vnstat_frontend\network\network_interface;
 use betterphp\vnstat_frontend\vnstat\vnstat;
 
 include('core/init.inc.php');
 
-$interfaces = vnstat::get_interfaces();
+$interfaces = network_interface::get_all();
 
 if (empty($interfaces)) {
 	die('No network interfaces detected.');
 }
 
-$selected_interface = (isset($_GET['interface']) && in_array($_GET['interface'], $interfaces))
-						? $_GET['interface']
-						: $interfaces[0];
+if (isset($_GET['interface'])) {
+	// Look for the selected interface if there is one
+	foreach ($interfaces as $test) {
+		if ($test->get_name() === $_GET['interface']) {
+			$interface = $test;
+			break;
+		}
+	}
+}
 
-$interface = new vnstat($selected_interface);
-$traffic = $interface->get_traffic();
+// Fall back to the first one on the list
+if (!isset($interface)) {
+	$interface = $interfaces[0];
+}
+
+$vnstat = $interface->get_vnstat();
 
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Traffic Stats for <?php echo $selected_interface; ?></title>
+		<title>Traffic Stats for <?php echo $interface->get_name(); ?></title>
 		<meta charset="utf-8" />
 		<link rel="stylesheet" type="text/css" media="all" href="ext/css/build/style.min.css" />
 		<script type="text/javascript">
-			var selectedInterface = '<?php echo $selected_interface; ?>';
+			var selectedInterface = '<?php echo $interface->get_name(); ?>';
 		</script>
 		<script type="text/javascript" src="https://www.google.com/jsapi"></script>
 		<script type="text/javascript" src="ext/jsc/build/script.min.js"></script>
@@ -41,10 +52,10 @@ $traffic = $interface->get_traffic();
 					<select name="interface">
 						<?php
 
-						foreach ($interfaces as $interface) {
-							$selected = ($interface == $selected_interface) ? ' selected="selected"' : '';
+						foreach ($interfaces as $option) {
+							$selected = ($option === $interface) ? ' selected="selected"' : '';
 
-							echo '<option ', $selected, '>', $interface, '</option>';
+							echo '<option ', $selected, '>', $option->get_name(), '</option>';
 						}
 
 						?>
@@ -71,38 +82,40 @@ $traffic = $interface->get_traffic();
 		<div class="main-content">
 			<?php
 
-			$entries = array(
-				array('key' => 'hours', 'format' => 'H:00'),
-				array('key' => 'days', 'format' => 'l jS F'),
-				array('key' => 'months', 'format' => 'F Y'),
-			);
+			$daily = $vnstat->get_daily_traffic();
+			$monthly = $vnstat->get_monthly_traffic();
 
-			foreach ($entries as $entry) {
-				if (empty($traffic[$entry['key']])) {
-					continue;
-				}
+			$charts = [
+				['get_hourly_traffic', 'hour', 'Hour', 'H:00'],
+				['get_daily_traffic', 'day', 'Day', 'l jS F'],
+				['get_monthly_traffic', 'month', 'Month', 'F Y'],
+			];
 
-				$name = substr($entry['key'], 0, -1);
+			foreach ($charts as $chart_spec) {
+				list($method_name, $id, $label, $date_format) = $chart_spec;
 
-				$chart_data = array();
-				$chart_data['cols'][] = array('id' => 'hour', 'label' => ucfirst($name), 'type' => 'datetime');
-				$chart_data['cols'][] = array('id' => 'rx', 'label' => 'Received', 'type' => 'number');
-				$chart_data['cols'][] = array('id' => 'tx', 'label' => 'Sent', 'type' => 'number');
+				$chart_data = [];
+				$chart_data['cols'][] = ['id' => $id, 'label' => $label, 'type' => 'datetime'];
+				$chart_data['cols'][] = ['id' => 'rx', 'label' => 'Received', 'type' => 'number'];
+				$chart_data['cols'][] = ['id' => 'tx', 'label' => 'Sent', 'type' => 'number'];
 
-				foreach ($traffic[$entry['key']] as $time => $data) {
+				foreach ($vnstat->$method_name() as $entry) {
+					$mbps_received = ($entry->get_received()->get_byte_rate() / 1024 / 1024);
+					$mbps_sent = ($entry->get_sent()->get_byte_rate() / 1024 / 1024);
+
 					$chart_data['rows'][] = [
 						'c' => [
 							[
-								'v' => 'Date(' . ($time * 1000) . ')',
-								'f' => date($entry['format'], $time),
+								'v' => 'Date(' . ($entry->get_start()->getTimestamp() * 1000) . ')',
+								'f' => $entry->get_start()->format($date_format),
 							],
 							[
-								'v' => round(($data['rx'] / 1024)),
-								'f' => number_format(round(($data['rx'] / 1024))) . ' MiB',
+								'v' => $mbps_received,
+								'f' => number_format($mbps_received) . ' MiB',
 							],
 							[
-								'v' => round(($data['tx'] / 1024)),
-								'f' => number_format(round(($data['tx'] / 1024))) . ' MiB',
+								'v' => $mbps_sent,
+								'f' => number_format($mbps_sent) . ' MiB',
 							],
 						]
 					];
@@ -110,11 +123,7 @@ $traffic = $interface->get_traffic();
 
 				?>
 				<div class="chart-wrapper">
-					<div
-						class="chart"
-						id="<?php echo $entry['key']; ?>-chart"
-						data-chart-data="<?php echo htmlentities(json_encode($chart_data)); ?>">
-					</div>
+					<div class="chart" data-chart-data="<?php echo htmlentities(json_encode($chart_data)); ?>"></div>
 				</div>
 				<?php
 			}
